27f51bdc237acf15166b8845bd79c2bd3df46c15
teojcryan <u2128816@live.warwick.ac.uk>
2022-06-15 14:20:10 +0800

    rename columns

--- code/get-data.R
+++ code/get-data.R
@@ -5,9 +5,12 @@ require(readr, quietly = TRUE)
 # nhse to LTLA lookup table
 ltla_nhser <- readRDS(here::here("data", "ltla_nhser.rds"))
 
-for (n in 1:10){
+n_delay <- 15 # days before current date
+
+# load data 
+for (n in 1:n_delay){
   
-  date <- Sys.Date()-n #gsub("-", "_", Sys.Date())
+  date = Sys.Date() - n
   
   if (!(format(date, "%A") %in% c("Saturday", "Sunday"))){
     cases_specimen_local <- read_csv(paste0(
@@ -32,7 +35,9 @@ for (n in 1:10){
       summarise(across(where(is.numeric), sum)) %>%
       ungroup() %>%
       rename(areaName = nhse_region) %>%
-      bind_rows(select(cases_specimen_national, -areaCode, -areaType))
+      bind_rows(select(cases_specimen_national, -areaCode, -areaType)) %>%
+      select(date, region = areaName, cases = newCasesBySpecimenDate) %>%
+      filter(!is.na(cases))
     
     saveRDS(cases_specimen, 
             here::here(paste0("data/cases_specimen_",
