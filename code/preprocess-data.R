99ab22eef6565bfbd2c01c7670aa43ccd63fa095
teojcryan <u2128816@live.warwick.ac.uk>
2022-08-04 13:57:19 +0100

    update file names

--- code/preprocess-data.R
+++ code/preprocess-data.R
@@ -1,47 +1,38 @@
+# Packages ----------------------------------------------------------------
 require(data.table, quietly = TRUE)
 require(stringr, quietly = TRUE)
 require(lubridate, quietly = TRUE, warn.conflicts = FALSE)
 suppressMessages(require(here, quietly = TRUE))
 
-# Pre-process data 
-
-# load data as list of data frames ------------- 
-filenames <- list.files(path = here("data", "observations", "national"), 
-                        pattern="*.rds", full.names=TRUE)
-ldf <- lapply(filenames, readRDS)
+# Load all raw data files -------------------------------------------------
+filenames <- list.files(path = here("data", "observations", "reported"), 
+                        pattern="*.rds", 
+                        full.names=TRUE)
+ldf <- lapply(filenames, readRDS) # load as list of data.tables
 names(ldf) <- str_extract(str_extract(filenames, "([^/]+$)"), ".*(?=\\.)") # set names as dates
 
+# merge list of data.tables into a single data.table
 dt <- data.table::rbindlist(ldf, idcol="report_date")     # combine list of data tables into single table
 date_start <- as.Date(min(names(ldf)), format="%Y-%m-%d") # earliest date of availability for archived data
-dt <- dt[date >= date_start]                              # filter after start date
+dt <- dt[specimen_date >= date_start]                              # filter after start date
 
 # rename specific dates
 dt[, `:=`(report_date = as.Date(report_date),
-          reference_date = date, 
+          reference_date = specimen_date, 
+          specimen_date = NULL,
           confirm = cases,
-          cases = NULL,
-          date = NULL)]
+          cases = NULL)]
 
 cases_cols <- c("confirm", "cases_lfd_pcr", "cases_lfd", "cases_pcr") # columns with case data
-setcolorder(dt, c("reference_date","report_date", cases_cols))      # reorder columns of data table
-dt <- dt[order(report_date, reference_date)]                        # sort data by specimen date
+setcolorder(dt, c("reference_date","report_date", cases_cols))        # reorder columns of data table
+dt <- dt[order(report_date, reference_date)]                          # sort data by specimen date
 
-# compute delay and incremental cases from cumulative
+# If data by update is required, compute delay and incremental cases from cumulative
 # dt[,':='(delay = as.numeric(report_date - specimen_date))
 # ][
 #   , by = c('specimen_date')
 #   , (cases_cols) := lapply(.SD, function(x) {ifelse(x == first(x), x, x - shift(x, type='lag'))})
 #   , .SDcols = cases_cols
 # ]
-# 
-# dt <- dt[delay > 0] # remove dates with same-day reporting or no change
-#dt <- dt[region == "England"] # filter only England data
 
 saveRDS(dt, here::here("data", "observations", "all.rds"))
-
-# synthetic weekly reported data ----------------------
-dt <- dt[, report_wday := wday(report_date)
-   ][report_wday == 4
-     ][, `:=`(report_wday = NULL)][]
-
-saveRDS(dt, here::here("data", "observations", "all_wkrep.rds"))
