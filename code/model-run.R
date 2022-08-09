a6b84dc82c8980aad792d5b2eae9ccb00b7ad973
teojcryan <u2128816@live.warwick.ac.uk>
2022-08-09 23:09:37 +0100

    remove model 3 to save time

--- code/model-run.R
+++ code/model-run.R
@@ -32,16 +32,17 @@ obs_all <- enw_complete_dates(obs_all, max_delay = d_max)
 
 # 1. Week day reporting, incl holidays
 obs_hol <- merge(obs_all, holidays, by.x = "report_date", by.y = "date")
+obs_hol[, weekend := ifelse(wday(report_date) %in% c(1,7), TRUE, FALSE)]
 
 # 2. Week day reporting, excl holidays
 obs_all[, holiday := FALSE]
-
   # apply variable to identify weekends
 obs_all[, weekend := ifelse(wday(report_date) %in% c(1,7), TRUE, FALSE)]
 
 # 3. Weekly reporting, excl holidays
 obs_wk <- enw_complete_dates(obs_wk, max_delay = d_max)
 obs_wk[, holiday := FALSE]
+obs_wk[, weekend := ifelse(wday(report_date) %in% c(1,7), TRUE, FALSE)]
 
   # include variable to track reporting dates (Wednesdays)
 obs_wk[, report_possible := ifelse(wday(report_date) == 4, TRUE, FALSE)]
@@ -49,9 +50,20 @@ obs_wk[, report_possible := ifelse(wday(report_date) == 4, TRUE, FALSE)]
 # Run models -------------------------------------------------------------
 # Set up multithreading
 ncores <- parallel::detectCores()
-nchains <- 4
+nchains <- 2
 threads <- ncores/nchains
 options(mc.cores = ncores)
+
+# Model fitting options
+fit <- enw_fit_opts(
+  save_warmup = FALSE, output_loglik = TRUE, pp = TRUE,
+  chains = nchains, threads_per_chain = threads, 
+  iter_sampling = 1000, iter_warmup = 1000,
+  show_messages = FALSE, refresh = 0, max_treedepth = 15, adapt_delta = .99
+)
+
+# Compile nowcasting model
+multithread_model <- enw_model(threads = TRUE, verbose = FALSE)
 
 time_start <- Sys.time()
 print(paste("Model fitting started at", time_start))
@@ -66,17 +78,6 @@ for (i in 1:length(date_list)){
   obs_hol_i <- filter_obs(obs_hol, date_nowcast, days_included)
   obs_wk_i <- filter_obs(obs_wk, date_nowcast, days_included)
   
-  # Model fitting options
-  fit <- enw_fit_opts(
-    save_warmup = FALSE, output_loglik = TRUE, pp = TRUE,
-    chains = nchains, threads_per_chain = threads, 
-    iter_sampling = 1000, iter_warmup = 1000,
-    show_messages = FALSE, refresh = 0, max_treedepth = 15, adapt_delta = .99
-  )
-  
-  # Compile nowcasting model
-  multithread_model <- enw_model(threads = TRUE, verbose = FALSE)
-  
   # Model 1: Reference fixed, report fixed
   cat(paste("===== Model 1 =====", "\n"))
   nowcast <- nowcast_model(obs_all_i, "fixed", d_max, fit, multithread_model)
@@ -86,8 +87,8 @@ for (i in 1:length(date_list)){
   wknd_nowcast <- nowcast_model(obs_all_i, "wknd", d_max, fit, multithread_model)
 
   # Model 3: Reference fixed, report day of week
-  cat(paste("===== Model 3 =====", "\n"))
-  dow_nowcast <- nowcast_model(obs_all_i, "dow", d_max, fit, multithread_model)
+  #cat(paste("===== Model 3 =====", "\n"))
+  #dow_nowcast <- nowcast_model(obs_all_i, "dow", d_max, fit, multithread_model)
   
   # Model 4: Reference fixed, report day of week + holidays
   cat(paste("===== Model 4 =====", "\n"))
@@ -101,7 +102,7 @@ for (i in 1:length(date_list)){
   nowcasts <- list(
     "Fixed" = nowcast,
     "Weekend" = wknd_nowcast,
-    "Dayofweek" = dow_nowcast,
+    # "Dayofweek" = dow_nowcast,
     "Holiday" = hol_nowcast,
     "Weekly" = wkly_nowcast
   )
