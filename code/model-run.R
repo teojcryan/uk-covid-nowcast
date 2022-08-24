ec6b07cfd18ab8ab4a8a9ed3ad4281861fb4f1d1
teojcryan <u2128816@live.warwick.ac.uk>
2022-08-24 07:07:50 +0100

    update parameters for model run 9d: day of week + hol reporting+ dow expectation

--- code/model-run.R
+++ code/model-run.R
@@ -112,7 +112,7 @@ for (i in 1:length(date_list)){
     # "Fixed" = nowcast,
     # "Weekend" = wknd_nowcast,
     # "Dayofweek" = dow_nowcast,
-    "Holiday" = hol_nowcast
+    "Holidaydow" = hol_nowcast
     # "Weekly" = wkly_nowcast
   )
   
